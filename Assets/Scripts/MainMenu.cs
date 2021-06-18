61ea7870348e488968194c5824e8f6673c4f97d8
AndrewPanibratow <55105407+AndrewPanibratow@users.noreply.github.com>
2021-06-18 06:01:14 +0200

    Game itself
    the game files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    private const float cameraRotationSpeed = 3.0f;
+    public GameObject levelButtonPrefab;
+    public GameObject levelButtonContainer;
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+    private void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+        foreach(Sprite thumbnail in thumbnails)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, cameraRotationSpeed * Time.deltaTime);
+        }
+    }
+    private void LoadLevel(string sceneName)
+    {
+        Debug.Log(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+        cameraDesiredLookAt = menuTransform;
+    }
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Change_Scene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
